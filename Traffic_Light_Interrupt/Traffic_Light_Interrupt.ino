d050dc21c03f86a8c397d4c00c9e52344e6093c4
Brandon Kirklen <brandonkirklen@gmail.com>
2015-09-28 19:18:38 -0700

    Uses Struct Now

--- Traffic_Light_Interrupt/Traffic_Light_Interrupt.ino
+++ Traffic_Light_Interrupt/Traffic_Light_Interrupt.ino
@@ -1,43 +1,55 @@
 volatile bool redState = false;
 volatile bool greenState = false;
 
+struct LightDef{
+  int lightPins[10];
+  int numberOfLights;
+};
+ 
 void setup() {
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
-
-  attachInterrupt(digitalPinToInterrupt(2), toggleRed, RISING);
+ 
+  attachInterrupt(digitalPinToInterrupt(2), toggleRed, RISING);   //Uses pins 2 and 3 as interrupt pins
   attachInterrupt(digitalPinToInterrupt(3), toggleGreen, RISING);
 }
-
+ 
+//controls state of red light
 void toggleRed() {
   redState = !redState;
 }
-
+ 
+//controls state of green light
 void toggleGreen() {
   greenState = !greenState;
 }
-
-void trafficLightReset(int lights[]){
-  for (int x = 0; x <= 3; x++){
-    digitalWrite( lights[x], LOW );
+ 
+//resets all pins in lights
+void trafficLightReset(struct LightDef lights){
+  for (int x = 0; x <= lights.numberOfLights; x++){
+    digitalWrite(lights.lightPins[x], LOW);
   }
 }
-
-void trafficLight(int lights[], int light, int duration){
+ 
+void trafficLight(struct LightDef lights, int light, int duration){
   trafficLightReset(lights);
   digitalWrite(light, HIGH);
   delay(duration);
 }
-
+ 
 void loop() {
   int lightRed = 6;
   int lightYellow = 5;
   int lightGreen = 4;
-  int lights[3] = {4,5,6};
-
+  struct LightDef lights;
+  lights.lightPins[0] = 4;
+  lights.lightPins[1] = 5;
+  lights.lightPins[2] = 6;
+  lights.numberOfLights = 3;
+ 
   if (redState){
     trafficLight(lights, lightRed, 1000);
   }
@@ -47,6 +59,6 @@ void loop() {
   else{
     trafficLight(lights, lightGreen, 2000);
     trafficLight(lights, lightYellow, 500);
-    trafficLight(lights, lightRed, 2000); 
+    trafficLight(lights, lightRed, 2000);
   }
 }
