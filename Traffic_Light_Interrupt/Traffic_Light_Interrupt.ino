813c984050dcf7e06ccdc35af747cbfe9c74ccc3
Brandon Kirklen <brandonkirklen@gmail.com>
2015-09-30 19:16:03 -0700

    Traffic Light Complete

--- Traffic_Light_Interrupt/Traffic_Light_Interrupt.ino
+++ Traffic_Light_Interrupt/Traffic_Light_Interrupt.ino
@@ -1,62 +1,65 @@
 volatile bool redState = false;
 volatile bool greenState = false;
 
-struct LightDef{
-  int lightPins[10];
-  int numberOfLights;
-};
- 
+namespace canttouchthis {
+  typedef struct {
+    int lightPins[10];
+    int numberOfLights;
+  } LightDef;
+}
+
+
+//resets all pins in lights
+void trafficLightReset(canttouchthis::LightDef lights) {
+  for (int x = 0; x <= lights.numberOfLights; x++) {
+    digitalWrite(lights.lightPins[x], LOW);
+  }
+}
+
+void trafficLight(canttouchthis::LightDef lights, int light, int duration) {
+  trafficLightReset(lights);
+  digitalWrite(light, HIGH);
+  delay(duration);
+}
+
+//controls state of red light
+void toggleRed() {
+  redState = !redState;
+}
+
+//controls state of green light
+void toggleGreen() {
+  greenState = !greenState;
+}
+
 void setup() {
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
- 
+
   attachInterrupt(digitalPinToInterrupt(2), toggleRed, RISING);   //Uses pins 2 and 3 as interrupt pins
   attachInterrupt(digitalPinToInterrupt(3), toggleGreen, RISING);
 }
- 
-//controls state of red light
-void toggleRed() {
-  redState = !redState;
-}
- 
-//controls state of green light
-void toggleGreen() {
-  greenState = !greenState;
-}
- 
-//resets all pins in lights
-void trafficLightReset(struct LightDef lights){
-  for (int x = 0; x <= lights.numberOfLights; x++){
-    digitalWrite(lights.lightPins[x], LOW);
-  }
-}
- 
-void trafficLight(struct LightDef lights, int light, int duration){
-  trafficLightReset(lights);
-  digitalWrite(light, HIGH);
-  delay(duration);
-}
- 
+
 void loop() {
   int lightRed = 6;
   int lightYellow = 5;
   int lightGreen = 4;
-  struct LightDef lights;
+  canttouchthis::LightDef lights;
   lights.lightPins[0] = lightRed;
   lights.lightPins[1] = lightYellow;
   lights.lightPins[2] = lightGreen;
   lights.numberOfLights = 3;
- 
-  if (redState){
+
+  if (redState) {
     trafficLight(lights, lightRed, 100);
   }
-  else if(greenState){
+  else if (greenState) {
     trafficLight(lights, lightGreen, 100);
   }
-  else{
+  else {
     trafficLight(lights, lightGreen, 2000);
     trafficLight(lights, lightYellow, 500);
     trafficLight(lights, lightRed, 2000);
