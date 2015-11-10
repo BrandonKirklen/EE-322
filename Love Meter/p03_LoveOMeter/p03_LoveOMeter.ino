bf7109443d13b71f89af8eb7c19393b33251f53f
Brandon Kirklen <brandonkirklen@gmail.com>
2015-11-09 20:12:09 -0800

    Lab Stuff

--- Love Meter/p03_LoveOMeter/p03_LoveOMeter.ino
+++ Love Meter/p03_LoveOMeter/p03_LoveOMeter.ino
@@ -2,16 +2,18 @@
 const int sensorPin = A0;
 // room temperature in Celcius
 const float baselineTemp = 28.0;
+bool print = false;
 
 void setup() {
   // open a serial connection to display values
   Serial.begin(9600);
+  pinMode(1, INPUT);
   // set the LED pins as outputs
   // the for() loop saves some extra coding
-  for (int pinNumber = 8; pinNumber <= 10; pinNumber++) {
-    pinMode(pinNumber, OUTPUT);
-    digitalWrite(pinNumber, LOW);
-  }
+//  for (int pinNumber = 8; pinNumber <= 10; pinNumber++) {
+//    pinMode(pinNumber, OUTPUT);
+//    digitalWrite(pinNumber, LOW);
+//  }
 }
 
 void loop() {
@@ -21,47 +23,51 @@ void loop() {
   int ledRed = 10;
   int ledYellow = 9;
   int ledGreen = 8;
+  int switchState = 0;
+  switchState = digitalRead(1);
 
-  // send the 10-bit sensor value out the serial port
+float voltage = (sensorVal / 1024.0) * 5.0;
+
+if( switchState == HIGH ){
+  print = !print;
+}
+
+if( print ){
   Serial.print("Sensor Value: ");
   Serial.print(sensorVal);
-
-  // convert the ADC reading to voltage
-  float voltage = (sensorVal / 1024.0) * 5.0;
-
-  // Send the voltage level out the Serial port
   Serial.print(", Volts: ");
   Serial.print(voltage);
+}
 
   // convert the voltage to temperature in degrees C
   // the sensor changes 10 mV per degree
   // the datasheet says there's a 500 mV offset
   // ((volatge - 500mV) times 100)
-  Serial.print(", degrees C: ");
-  float temperature = (voltage - .5) * 100;
-  Serial.println(temperature);
+//  Serial.print(", degrees C: ");
+//  float temperature = (voltage - .5) * 100;
+//  Serial.println(temperature);
 
   // if the current temperature is lower than the baseline
   // turn off all LEDs
-  if (temperature < baselineTemp + 2) {
-    digitalWrite(ledRed, LOW);
-    digitalWrite(ledYellow, LOW);
-    digitalWrite(ledGreen, LOW);
-  } // if the temperature rises 2-4 degrees, turn an LED on
-  else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4) {
-    digitalWrite(ledRed, HIGH);
-    digitalWrite(ledYellow, LOW);
-    digitalWrite(ledGreen, LOW);
-  } // if the temperature rises 4-6 degrees, turn a second LED on
-  else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6) {
-    digitalWrite(ledRed, HIGH);
-    digitalWrite(ledYellow, HIGH);
-    digitalWrite(ledGreen, LOW);
-  } // if the temperature rises more than 6 degrees, turn all LEDs on
-  else if (temperature >= baselineTemp + 6) {
-    digitalWrite(ledRed, HIGH);
-    digitalWrite(ledYellow, HIGH);
-    digitalWrite(ledGreen, HIGH);
-  }
+//  if (temperature < baselineTemp + 2) {
+//    digitalWrite(ledRed, LOW);
+//    digitalWrite(ledYellow, LOW);
+//    digitalWrite(ledGreen, LOW);
+//  } // if the temperature rises 2-4 degrees, turn an LED on
+//  else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4) {
+//    digitalWrite(ledRed, HIGH);
+//    digitalWrite(ledYellow, LOW);
+//    digitalWrite(ledGreen, LOW);
+//  } // if the temperature rises 4-6 degrees, turn a second LED on
+//  else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6) {
+//    digitalWrite(ledRed, HIGH);
+//    digitalWrite(ledYellow, HIGH);
+//    digitalWrite(ledGreen, LOW);
+//  } // if the temperature rises more than 6 degrees, turn all LEDs on
+//  else if (temperature >= baselineTemp + 6) {
+//    digitalWrite(ledRed, HIGH);
+//    digitalWrite(ledYellow, HIGH);
+//    digitalWrite(ledGreen, HIGH);
+//  }
   delay(1);
 }
